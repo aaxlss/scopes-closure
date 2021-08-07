f6be8c51c69d939a1706552c539e40f4f8a75fa4
aaxlss <axl9305@gmail.com>
2021-08-07 23:32:40 +0100

    Difference between let, var and const in blocks of code

---
+++ src/scope/block.js
@@ -0,0 +1,32 @@
+const fruits = () => {
+    if(true){
+        var fruit1 = 'apple';
+        let fruit2 = 'banana';//las variables let no pueden ser accedidas desde fuera del bloque de codigo al que pertenecen
+        const fruit3 = 'kiwi';//las constantes no pueden ser accedidas desde fuera del bloque de codigo al que pertenecen
+
+    }
+
+    console.log(fruit1);
+    console.log(fruit2);//no puede imprimir el valor
+    console.log(fruit3);//no puede imprimir el valor
+
+}
+
+fruits();
+
+
+let x = 1;
+{
+    let x = 2;
+    console.log(x);
+}
+console.log(x);
+
+const anotherFunction = () {
+    for (let index = 0; index < 10; index++) {
+        setTimeout(()=>{
+            console.log(index)
+        },1000);
+        
+    }
+}
