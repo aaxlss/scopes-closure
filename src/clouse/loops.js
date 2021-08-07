58fa64e952452ca55185b15eda9b1ccb31ac2552
aaxlss <axl9305@gmail.com>
2021-08-08 00:08:49 +0100

    closure inside of loops

---
+++ src/clouse/loops.js
@@ -0,0 +1,9 @@
+const anotherFunction = () => {
+    for (let index = 0; index < 10; index++) {
+        setTimeout(()=>{
+            console.log(index)
+        },1000);
+        
+    }
+}
+anotherFunction();
